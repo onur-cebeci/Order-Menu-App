95cbad5c425eb862b5310069670e52adcb2ece8c
onur-cebeci <112099545+onur-cebeci@users.noreply.github.com>
2022-10-03 09:51:37 +0300

    I creted constant value

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,26 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            fontSize: 45,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+          headline2: TextStyle(
+            fontSize: 35,
+            fontWeight: FontWeight.w500,
+            color: Colors.white,
+          ),
+          headline3: TextStyle(
+            fontSize: 28,
+            color: Colors.white,
+          ),
+          headline4: TextStyle(
+            fontSize: 24,
+            color: Colors.white,
+          ),
+        ),
       ),
       home: const HomePage(),
     );
--- lib/screens/home_pages/home_page.dart
+++ lib/screens/home_pages/home_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:order_web_app/constant.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(
+        gradient: backgroundGradient,
+      ),
+    );
   }
 }
